8f8de685564563b7f96e205e4b27c8ff8805c9db
Laura Benavidez <Laurabr314@gmail.com>
2022-01-18 11:21:06 -0300

    creamos un programa que pregunte nombre y edad de dos personas

---
+++ datos_personas.py
@@ -0,0 +1,18 @@
+from os import system
+
+nombre_1 = input('Escribe tu nombre: ')
+system('cls')
+nombre_2 = input('Escribe tu nombre: ')
+system('cls')
+
+edad_1 = int(input(f'{nombre_1} indica que edad tienes: '))
+system('cls')
+edad_2 = int(input(f'{nombre_2} Indica que edad tienes: '))
+system('cls')
+
+if edad_1 > edad_2:
+    print(nombre_1 + ' es mayor que ' + nombre_2)
+elif edad_1 < edad_2:
+    print(nombre_2 + ' es mayor que ' + nombre_1)
+else:
+    print('Ambos tienen la misma edad')
